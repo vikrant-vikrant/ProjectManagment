756e8005110869423a4322ac4f295c5c969add3f
vikrant <vikrant2003vikrant@gmail.com>
2026-09-17 23:40:24 +0530

    feat: add CORS support and ApiResponse utility class

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 import express from "express";
-
+import cors from "cors";
 const app = express();
 
+//basic configration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//CORS configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.get("/", (req, res) => {
-  res.send("welcome home");
+  // res.send("welcome home");
+  res.send(res.status);
+
 });
 app.get("/about", (req, res) => {
   res.send("welcome about section");
