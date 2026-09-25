ffb5b2f1d8d48154c59089714b04039c8531b3ba
vikrant <vikrant2003vikrant@gmail.com>
2026-09-25 20:20:40 +0530

    feat: add user registration validation and error handling middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import { userRegisterValidator } from "../validators/index.js";
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
 export default router;
---
+++ src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+    body("Username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lower case")
+      .isLength({ min: 3 })
+      .withMessage("Username must be at least 3 characters"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("fullname").optional().trim(),
+  ];
+};
+
+export { userRegisterValidator };
